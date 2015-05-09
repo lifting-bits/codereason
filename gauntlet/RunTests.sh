2a78cda9975a44c542eb8f4170d7da6cbe0d83a6
Luke <lmladek771@gmail.com>
2015-05-08 22:05:12 -0400

    fixed wrong path to Results

--- gauntlet/RunTests.sh
+++ gauntlet/RunTests.sh
@@ -22,7 +22,7 @@ RESULT=0
 #########################################x86 tests #################################################
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/call_reg.lua | tail -n +7 >  tmp
-cmp tmp Results/Test1
+cmp tmp ../MachO-Test-Binaries/Results/Test1
 if [ $? -eq 1 ]
 then
   echo "Test1 failed:"
@@ -31,7 +31,7 @@ then
 fi
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/pop_ecx.lua | tail -n +7 >  tmp
-cmp tmp Results/Test2
+cmp tmp ../MachO-Test-Binaries/Results/Test2
 
 if [ $? -eq 1 ]
 then
@@ -42,7 +42,7 @@ fi
 
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/pvtdown.lua | tail -n +7 >  tmp
-cmp tmp Results/Test3
+cmp tmp ../MachO-Test-Binaries/Results/Test3
 
 if [ $? -eq 1 ]
 then
@@ -52,7 +52,7 @@ then
 fi
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/pvtfinder.lua | tail -n +7 >  tmp
-cmp tmp Results/Test4
+cmp tmp ../MachO-Test-Binaries/Results/Test4
 
 if [ $? -eq 1 ]
 then
@@ -62,7 +62,7 @@ then
 fi
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/pvtfinder2.lua | tail -n +7 >  tmp
-cmp tmp Results/Test5
+cmp tmp ../MachO-Test-Binaries/Results/Test5
 
 if [ $? -eq 1 ]
 then
@@ -72,7 +72,7 @@ then
 fi
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/vcall.lua | tail -n +7 >  tmp
-cmp tmp Results/Test6
+cmp tmp ../MachO-Test-Binaries/Results/Test6
 
 if [ $? -eq 1 ]
 then
@@ -82,7 +82,7 @@ then
 fi
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/vcall2.lua | tail -n +7 >  tmp
-cmp tmp Results/Test7
+cmp tmp ../MachO-Test-Binaries/Results/Test7
 
 if [ $? -eq 1 ]
 then
@@ -92,7 +92,7 @@ then
 fi
 
 ./../build/bin/RopTool -f ../MachO-Test-Binaries/x86/MachO-OSX-x86-ls -c ../scripts/x86/transfer_arith_with_branch.lua | tail -n +7 >  tmp
-cmp tmp Results/Test8
+cmp tmp ../MachO-Test-Binaries/Results/Test8
 
 if [ $? -eq 1 ]
 then
@@ -104,7 +104,7 @@ fi
 ###############x64 tests ( ommitted all filezilla tests due to their size)  #####################################################
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/cat | tail -n +7 >  tmp
-cmp tmp Results/Test9
+cmp tmp ../MachO-Test-Binaries/Results/Test9
 
 if [ $? -eq 1 ]
 then
@@ -114,7 +114,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/chmod | tail -n +7 >  tmp
-cmp tmp Results/Test10
+cmp tmp ../MachO-Test-Binaries/Results/Test10
 
 if [ $? -eq 1 ]
 then
@@ -124,7 +124,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/cp | tail -n +7 >  tmp
-cmp tmp Results/Test11
+cmp tmp ../MachO-Test-Binaries/Results/Test11
 
 if [ $? -eq 1 ]
 then
@@ -134,7 +134,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/csh | tail -n +7 >  tmp
-cmp tmp Results/Test12
+cmp tmp ../MachO-Test-Binaries/Results/Test12
 
 if [ $? -eq 1 ]
 then
@@ -144,7 +144,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/date | tail -n +7 >  tmp
-cmp tmp Results/Test13
+cmp tmp ../MachO-Test-Binaries/Results/Test13
 
 if [ $? -eq 1 ]
 then
@@ -154,7 +154,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/dd | tail -n +7 >  tmp
-cmp tmp Results/Test14
+cmp tmp ../MachO-Test-Binaries/Results/Test14
 
 if [ $? -eq 1 ]
 then
@@ -164,7 +164,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/df | tail -n +7 >  tmp
-cmp tmp Results/Test15
+cmp tmp ../MachO-Test-Binaries/Results/Test15
 
 if [ $? -eq 1 ]
 then
@@ -174,7 +174,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/domainname | tail -n +7 >  tmp
-cmp tmp Results/Test16
+cmp tmp ../MachO-Test-Binaries/Results/Test16
 
 if [ $? -eq 1 ]
 then
@@ -184,7 +184,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/echo | tail -n +7 > tmp
-cmp tmp Results/Test17
+cmp tmp ../MachO-Test-Binaries/Results/Test17
 
 if [ $? -eq 1 ]
 then
@@ -194,7 +194,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/ed | tail -n +7 >  tmp
-cmp tmp Results/Test18
+cmp tmp ../MachO-Test-Binaries/Results/Test18
 
 if [ $? -eq 1 ]
 then
@@ -204,7 +204,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/expr | tail -n +7 >  tmp
-cmp tmp Results/Test19
+cmp tmp ../MachO-Test-Binaries/Results/Test19
 
 if [ $? -eq 1 ]
 then
@@ -214,7 +214,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/hostname | tail -n +7 >  tmp
-cmp tmp Results/Test20
+cmp tmp ../MachO-Test-Binaries/Results/Test20
 
 if [ $? -eq 1 ]
 then
@@ -224,7 +224,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/kill | tail -n +7 >  tmp
-cmp tmp Results/Test21
+cmp tmp ../MachO-Test-Binaries/Results/Test21
 
 if [ $? -eq 1 ]
 then
@@ -235,7 +235,7 @@ fi
 
 #Failed Core Dumped
 #./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/ksh | tail -n +7 >  tmp
-#cmp tmp Results/Test22
+#cmp tmp ../MachO-Test-Binaries/Results/Test22
 #
 #if [ $? -eq 1 ]
 #then
@@ -244,7 +244,7 @@ fi
 
 #Failed Core Dumped
 #./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/launchctl | tail -n +7 >  tmp
-#cmp tmp Results/Test23
+#cmp tmp ../MachO-Test-Binaries/Results/Test23
 #
 #if [ $? -eq 1 ]
 #then
@@ -252,7 +252,7 @@ fi
 #fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/link | tail -n +7 > tmp
-cmp tmp Results/Test24
+cmp tmp ../MachO-Test-Binaries/Results/Test24
 
 if [ $? -eq 1 ]
 then
@@ -262,7 +262,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/ln  | tail -n +7 >  tmp
-cmp tmp Results/Test25
+cmp tmp ../MachO-Test-Binaries/Results/Test25
 
 if [ $? -eq 1 ]
 then
@@ -272,7 +272,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/ls | tail -n +7 >  tmp
-cmp tmp Results/Test26
+cmp tmp ../MachO-Test-Binaries/Results/Test26
 
 if [ $? -eq 1 ]
 then
@@ -282,7 +282,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/MachO-OSX-x64-ls | tail -n +7 > tmp
-cmp tmp Results/Test27
+cmp tmp ../MachO-Test-Binaries/Results/Test27
 
 if [ $? -eq 1 ]
 then
@@ -292,7 +292,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/mkdir | tail -n +7 >  tmp
-cmp tmp Results/Test28
+cmp tmp ../MachO-Test-Binaries/Results/Test28
 
 if [ $? -eq 1 ]
 then
@@ -302,7 +302,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/mv | tail -n +7 >  tmp
-cmp tmp Results/Test29
+cmp tmp ../MachO-Test-Binaries/Results/Test29
 
 if [ $? -eq 1 ]
 then
@@ -312,7 +312,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/pax | tail -n +7 >  tmp
-cmp tmp Results/Test30
+cmp tmp ../MachO-Test-Binaries/Results/Test30
 
 if [ $? -eq 1 ]
 then
@@ -322,7 +322,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/ps | tail -n +7 >  tmp
-cmp tmp Results/Test31
+cmp tmp ../MachO-Test-Binaries/Results/Test31
 
 if [ $? -eq 1 ]
 then
@@ -332,7 +332,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/pwd | tail -n +7 >  tmp
-cmp tmp Results/Test32
+cmp tmp ../MachO-Test-Binaries/Results/Test32
 
 if [ $? -eq 1 ]
 then
@@ -342,7 +342,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/rcp | tail -n +7 >  tmp
-cmp tmp Results/Test33
+cmp tmp ../MachO-Test-Binaries/Results/Test33
 
 if [ $? -eq 1 ]
 then
@@ -352,7 +352,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/rm | tail -n +7 >  tmp
-cmp tmp Results/Test34
+cmp tmp ../MachO-Test-Binaries/Results/Test34
 
 if [ $? -eq 1 ]
 then
@@ -362,7 +362,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/rmdir | tail -n +7 >  tmp
-cmp tmp Results/Test35
+cmp tmp ../MachO-Test-Binaries/Results/Test35
 
 if [ $? -eq 1 ]
 then
@@ -372,7 +372,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/sleep | tail -n +7 >  tmp
-cmp tmp Results/Test36
+cmp tmp ../MachO-Test-Binaries/Results/Test36
 
 if [ $? -eq 1 ]
 then
@@ -382,7 +382,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/stty | tail -n +7 >  tmp
-cmp tmp Results/Test37
+cmp tmp ../MachO-Test-Binaries/Results/Test37
 
 if [ $? -eq 1 ]
 then
@@ -392,7 +392,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/tcsh | tail -n +7 >  tmp
-cmp tmp Results/Test38
+cmp tmp ../MachO-Test-Binaries/Results/Test38
 
 if [ $? -eq 1 ]
 then
@@ -402,7 +402,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/test | tail -n +7 > tmp
-cmp tmp Results/Test39
+cmp tmp ../MachO-Test-Binaries/Results/Test39
 
 if [ $? -eq 1 ]
 then
@@ -412,7 +412,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/unlink | tail -n +7 > tmp
-cmp tmp Results/Test40
+cmp tmp ../MachO-Test-Binaries/Results/Test40
 
 if [ $? -eq 1 ]
 then
@@ -422,7 +422,7 @@ then
 fi
 
 ./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/wait4path | tail -n +7 > tmp
-cmp tmp Results/Test41
+cmp tmp ../MachO-Test-Binaries/Results/Test41
 
 if [ $? -eq 1 ]
 then
@@ -433,7 +433,7 @@ fi
 
 #Failed Core Dumped
 #./../build/bin/RopTool -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x64/zsh | tail -n +7 >  tmp
-#cmp tmp Results/Test42
+#cmp tmp ../MachO-Test-Binaries/Results/Test42
 #
 #if [ $? -eq 1 ]
 #then
@@ -446,14 +446,14 @@ fi
 ########  As x86 ##############
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/call_reg.lua | tail -n +7 >  tmp
-#cmp tmp Results/Test43
+#cmp tmp ../MachO-Test-Binaries/Results/Test43
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
 #fi
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/pop_ecx.lua | tail -n +7 > tmp
-#cmp tmp Results/Test44
+#cmp tmp ../MachO-Test-Binaries/Results/Test44
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -461,7 +461,7 @@ fi
 
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/pvtdown.lua | tail -n +7 > tmp
-cmp tmp Results/Test45
+cmp tmp ../MachO-Test-Binaries/Results/Test45
 if [ $? -eq 1 ]
 then
   echo "Test45 failed:"
@@ -469,7 +469,7 @@ then
   RESULT=1
 fi
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/pvtfinder.lua | tail -n +7 > tmp
-#cmp tmp Results/Test46
+#cmp tmp ../MachO-Test-Binaries/Results/Test46
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -477,13 +477,13 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/pvtfinder2.lua | tail -n +7 > tmp
-#cmp tmp Results/Test47
+#cmp tmp ../MachO-Test-Binaries/Results/Test47
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
 #fi
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/vcall.lua | tail -n +7 > tmp
-#cmp tmp Results/Test48
+#cmp tmp ../MachO-Test-Binaries/Results/Test48
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -491,7 +491,7 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/vcall2.lua | tail -n +7 > tmp
-#cmp tmp Results/Test49
+#cmp tmp ../MachO-Test-Binaries/Results/Test49
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -500,7 +500,7 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/bash -c ../scripts/x86/transfer_arith_with_branch.lua | tail -n +7 > tmp
-#cmp tmp Results/Test50
+#cmp tmp ../MachO-Test-Binaries/Results/Test50
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -509,7 +509,7 @@ fi
 
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/call_reg.lua | tail -n +7 >  tmp
-cmp tmp Results/Test51
+cmp tmp ../MachO-Test-Binaries/Results/Test51
 if [ $? -eq 1 ]
 then
   echo "Test51 failed:"
@@ -519,7 +519,7 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/pop_ecx.lua | tail -n +7 > tmp
-#cmp tmp Results/Test52
+#cmp tmp ../MachO-Test-Binaries/Results/Test52
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -527,7 +527,7 @@ fi
 
 
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/pvtdown.lua | tail -n +7 > tmp
-#cmp tmp Results/Test53
+#cmp tmp ../MachO-Test-Binaries/Results/Test53
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -535,14 +535,14 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/pvtfinder.lua | tail -n +7 > tmp
-#cmp tmp Results/Test54
+#cmp tmp ../MachO-Test-Binaries/Results/Test54
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
 #fi
 
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/pvtfinder2.lua | tail -n +7 > tmp
-#cmp tmp Results/Test55
+#cmp tmp ../MachO-Test-Binaries/Results/Test55
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -550,14 +550,14 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/vcall.lua | tail -n +7 > tmp
-#cmp tmp Results/Test56
+#cmp tmp ../MachO-Test-Binaries/Results/Test56
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
 #fi
 
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/vcall2.lua | tail -n +7 > tmp
-#cmp tmp Results/Test57
+#cmp tmp ../MachO-Test-Binaries/Results/Test57
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -565,14 +565,14 @@ fi
 
 # Too Slow
 #./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sh -c ../scripts/x86/transfer_arith_with_branch.lua | tail -n +7 > tmp
-#cmp tmp Results/Test58
+#cmp tmp ../MachO-Test-Binaries/Results/Test58
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
 #fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/call_reg.lua | tail -n +7 > tmp
-cmp tmp Results/Test59
+cmp tmp ../MachO-Test-Binaries/Results/Test59
 if [ $? -eq 1 ]
 then
   echo "Test59 failed:"
@@ -581,7 +581,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/pop_ecx.lua | tail -n +7 > tmp
-cmp tmp Results/Test60
+cmp tmp ../MachO-Test-Binaries/Results/Test60
 if [ $? -eq 1 ]
 then
   echo "Test60 failed:"
@@ -590,7 +590,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/pvtdown.lua | tail -n +7 > tmp
-cmp tmp Results/Test61
+cmp tmp ../MachO-Test-Binaries/Results/Test61
 if [ $? -eq 1 ]
 then
   echo "Test61 failed:"
@@ -599,7 +599,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/pvtfinder.lua | tail -n +7 > tmp
-cmp tmp Results/Test62
+cmp tmp ../MachO-Test-Binaries/Results/Test62
 if [ $? -eq 1 ]
 then
   echo "Test62 failed:"
@@ -608,7 +608,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/pvtfinder2.lua | tail -n +7 > tmp
-cmp tmp Results/Test63
+cmp tmp ../MachO-Test-Binaries/Results/Test63
 if [ $? -eq 1 ]
 then
   echo "Test63 failed:"
@@ -617,7 +617,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/vcall.lua | tail -n +7 > tmp
-cmp tmp Results/Test64
+cmp tmp ../MachO-Test-Binaries/Results/Test64
 if [ $? -eq 1 ]
 then
   echo "Test64 failed:"
@@ -626,7 +626,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/vcall2.lua | tail -n +7 > tmp
-cmp tmp Results/Test65
+cmp tmp ../MachO-Test-Binaries/Results/Test65
 if [ $? -eq 1 ]
 then
   echo "Test65 failed:"
@@ -635,7 +635,7 @@ then
 fi
 
 ./../build/bin/RopTool -a x86 -f ../MachO-Test-Binaries/x86_64_FAT/sync -c ../scripts/x86/transfer_arith_with_branch.lua | tail -n +7 > tmp
-cmp tmp Results/Test66
+cmp tmp ../MachO-Test-Binaries/Results/Test66
 if [ $? -eq 1 ]
 then
   echo "Test66 failed:"
@@ -649,7 +649,7 @@ fi
 
 # Hang Up / Freeze
 #./../build/bin/RopTool -a x64 -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x86_64_FAT/bash | tail -n +7 > tmp
-#cmp tmp Results/Test67
+#cmp tmp ../MachO-Test-Binaries/Results/Test67
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -657,7 +657,7 @@ fi
 
 # Hang Up / Freeze
 #./../build/bin/RopTool -a x64 -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x86_64_FAT/sh | tail -n +7 > tmp
-#cmp tmp Results/Test68
+#cmp tmp ../MachO-Test-Binaries/Results/Test68
 #if [ $? -eq 1 ]
 #then
 #  RESULT=1
@@ -665,7 +665,7 @@ fi
 
 
 ./../build/bin/RopTool -a x64 -c ../scripts/x64/call_reg.lua -f ../MachO-Test-Binaries/x86_64_FAT/sync | tail -n +7 > tmp
-cmp tmp  Results/Test69
+cmp tmp  ../MachO-Test-Binaries/Results/Test69
 if [ $? -eq 1 ]
 then
   echo "Test69 failed:"
