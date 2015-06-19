82081f4c756a074bf860f0ef2b4a2cdbbedfd72f
gaasedelen <markus.gaasedelen@gmail.com>
2015-06-18 19:00:35 -0700

    cleaned up tabs/spaces

--- libs/IR/decodeToIR.cpp
+++ libs/IR/decodeToIR.cpp
@@ -20,13 +20,13 @@ pthread_key_t jBuffTls;
 #endif
 
 struct DecodeLibState {
-	VexControl	vcon;
-	VexArchInfo	vai_guest;
+    VexControl  vcon;
+    VexArchInfo vai_guest;
     VexArchInfo vai_host;
-	VexAbiInfo	vbi;
-	VexArch		guestarch;
-	TargetInfo	ti;
-	bool        do_throw;
+    VexAbiInfo  vbi;
+    VexArch     guestarch;
+    TargetInfo  ti;
+    bool        do_throw;
 };
 
 
@@ -47,15 +47,15 @@ typedef IRSB *(*vexInstTy)(void *,IRSB *,const VexGuestLayout *,const VexGuestEx
 
 struct TransContext
 {
-	vector<BlockPtr>	&blocks;
+    vector<BlockPtr>    &blocks;
 
-	bool isStmtInCtx(Statement *st) {
-		return false;
-	}
+    bool isStmtInCtx(Statement *st) {
+        return false;
+    }
 
-	bool isBlockInCtx(BlockPtr b) {
-		return false;
-	}
+    bool isBlockInCtx(BlockPtr b) {
+        return false;
+    }
 
     TransContext(vector<BlockPtr> &b) : blocks(b) { }
 
@@ -64,19 +64,19 @@ struct TransContext
 
 struct TransContext2
 {
-	vector<BlockPtr>	&blocks;
-	unsigned long   curId;
-	TargetArch		targetArch;
+    vector<BlockPtr>    &blocks;
+    unsigned long   curId;
+    TargetArch      targetArch;
 
-	bool isStmtInCtx(Statement *st) {
-		return false;
-	}
+    bool isStmtInCtx(Statement *st) {
+        return false;
+    }
 
-	bool isBlockInCtx(BlockPtr b) {
-		return false;
-	}
+    bool isBlockInCtx(BlockPtr b) {
+        return false;
+    }
 
-	TransContext2(vector<BlockPtr> &b) : blocks(b) { return; }
+    TransContext2(vector<BlockPtr> &b) : blocks(b) { return; }
 
     TargetArch  ta;
 };
@@ -84,18 +84,18 @@ struct TransContext2
 
 struct OneBlockTransCtx
 {
-	BlockPtr        b;
-	unsigned long   curId;
+    BlockPtr        b;
+    unsigned long   curId;
     TargetArch      ta;
     unsigned int    maxSize;
 };
 
 class TranslateException {
-	std::string	msg;
+    std::string msg;
 public:
-	TranslateException(std::string m) {
-		this->msg = m;
-	}
+    TranslateException(std::string m) {
+        this->msg = m;
+    }
 };
 
 #ifndef WIN32
@@ -131,8 +131,8 @@ __attribute__((noreturn))
 #endif
 void failure_exit ( void )
 {
-	fprintf(stdout, "VEX did failure_exit.  Bye.\n");
-	exit(1);
+    fprintf(stdout, "VEX did failure_exit.  Bye.\n");
+    exit(1);
 }
 
 static
@@ -140,23 +140,23 @@ static
 __attribute__((noreturn))
 #endif
 void failure_throw(void) {
-	//throw an exception to get us back to someone that cares
-	throw TranslateException("failure_throw called");
+    //throw an exception to get us back to someone that cares
+    throw TranslateException("failure_throw called");
 }
 
 static
 void log_bytes ( const HChar* bytes, SizeT nbytes )
 {
-	//char b[] = { 'V', 'L', 'O', 'G', '>', ' '};
-	//fwrite ( b, 1, sizeof(b), stdout );
-	fwrite ( bytes, 1, nbytes, stdout );
-	return;
+    //char b[] = { 'V', 'L', 'O', 'G', '>', ' '};
+    //fwrite ( b, 1, sizeof(b), stdout );
+    fwrite ( bytes, 1, nbytes, stdout );
+    return;
 }
 
 static
 void eat_bytes( const HChar *bytes, SizeT nbytes ) 
 {
-	return;
+    return;
 }
 
 static 
@@ -166,36 +166,36 @@ UInt needs_self_check ( void* opaque, VexRegisterUpdates * vru, const VexGuestEx
 
 void initVex(DecodeLibState *dls, bool do_throw, bool dbg_spew) {
 
-	LibVEX_default_VexControl(&dls->vcon);
-	dls->vcon.iropt_level = dls->ti.opLevel; //max optimization
-	dls->vcon.guest_max_insns = dls->ti.maxInstructions; //number of insns in a guest block;
-	dls->vcon.guest_chase_thresh = dls->ti.chaseThreshold;
+    LibVEX_default_VexControl(&dls->vcon);
+    dls->vcon.iropt_level = dls->ti.opLevel; //max optimization
+    dls->vcon.guest_max_insns = dls->ti.maxInstructions; //number of insns in a guest block;
+    dls->vcon.guest_chase_thresh = dls->ti.chaseThreshold;
 
-	LibVEX_default_VexArchInfo(&dls->vai_guest);
-	LibVEX_default_VexArchInfo(&dls->vai_host);
+    LibVEX_default_VexArchInfo(&dls->vai_guest);
+    LibVEX_default_VexArchInfo(&dls->vai_host);
 
-	dls->vai_guest.hwcaps = dls->ti.guestHWcaps;
-	dls->vai_host.hwcaps = dls->ti.hostHWcaps;
+    dls->vai_guest.hwcaps = dls->ti.guestHWcaps;
+    dls->vai_host.hwcaps = dls->ti.hostHWcaps;
 
-	LibVEX_default_VexAbiInfo(&dls->vbi);
+    LibVEX_default_VexAbiInfo(&dls->vbi);
     dls->vbi.guest_stack_redzone_size = 128;
 
-	dls->do_throw = do_throw;
-	if( do_throw ) {
-		if( dbg_spew ) {
-			LibVEX_Init(failure_throw, log_bytes, 1, &dls->vcon);
-		} else {
-			LibVEX_Init(failure_throw, eat_bytes, 1, &dls->vcon);
-		}
-	} else {
-		if( dbg_spew ){
-			LibVEX_Init(failure_exit, log_bytes, 1, &dls->vcon);
-		} else {
-			LibVEX_Init(failure_exit, eat_bytes, 1, &dls->vcon);
-		}
-	}
+    dls->do_throw = do_throw;
+    if( do_throw ) {
+        if( dbg_spew ) {
+            LibVEX_Init(failure_throw, log_bytes, 1, &dls->vcon);
+        } else {
+            LibVEX_Init(failure_throw, eat_bytes, 1, &dls->vcon);
+        }
+    } else {
+        if( dbg_spew ){
+            LibVEX_Init(failure_exit, log_bytes, 1, &dls->vcon);
+        } else {
+            LibVEX_Init(failure_exit, eat_bytes, 1, &dls->vcon);
+        }
+    }
 
-	return;
+    return;
 }
 
 /* we can get pointer to vector of statements
@@ -208,11 +208,11 @@ __cdecl
 #endif
 convertToOneBlockCb(void *ctx,
                     IRSB *bb_in,
-				    const VexGuestLayout *vgl,
-				    const VexGuestExtents *vge,
-				    const VexArchInfo *vgi, // in new valgrind, currently used. TODO see if we should
-				    IRType girty,
-				    IRType hirty)
+                    const VexGuestLayout *vgl,
+                    const VexGuestExtents *vge,
+                    const VexArchInfo *vgi, // in new valgrind, currently used. TODO see if we should
+                    IRType girty,
+                    IRType hirty)
 {
     OneBlockTransCtx *obtc = (OneBlockTransCtx *)ctx;
 
@@ -222,37 +222,37 @@ convertToOneBlockCb(void *ctx,
         obtc->b = boost::static_pointer_cast<Block>(b);
     }
 
-	return bb_in;
+    return bb_in;
 }
 
 bool runVEXOnBlobWithCallback(DecodeLibState *dls,
-							  unsigned char	*b,
-                              unsigned long	bufExtents,
+                              unsigned char *b,
+                              unsigned long bufExtents,
                               uint64_t b_addr,
-                              vexInstTy	func,
+                              vexInstTy func,
                               void *ctx,
-                              unsigned long	*readBytes,
+                              unsigned long *readBytes,
                               TargetArch arch)
 {
-	VexTranslateArgs vta;
-	VexTranslateResult vtr;
-	VexGuestExtents	vge;
-	bool didGood = false;
+    VexTranslateArgs vta;
+    VexTranslateResult vtr;
+    VexGuestExtents vge;
+    bool didGood = false;
 
-	memset(&vta, 0, sizeof(VexTranslateArgs));
-    memset(&vge, 0, sizeof(VexGuestExtents));	
+    memset(&vta, 0, sizeof(VexTranslateArgs));
+    memset(&vge, 0, sizeof(VexGuestExtents));   
 
-	vta.instrument1 = func;
-	vta.callback_opaque = ctx;
+    vta.instrument1 = func;
+    vta.callback_opaque = ctx;
 
-	vta.abiinfo_both = dls->vbi;
-	vta.chase_into_ok = chase_into_not_ok;
+    vta.abiinfo_both = dls->vbi;
+    vta.chase_into_ok = chase_into_not_ok;
 
     vta.guest_bytes = b;
     vta.guest_bytes_addr = (Addr64)b_addr;
-	vta.guest_extents = &vge;
-	vta.archinfo_guest = dls->vai_guest;
-	vta.archinfo_guest.endness = VexEndnessLE;
+    vta.guest_extents = &vge;
+    vta.archinfo_guest = dls->vai_guest;
+    vta.archinfo_guest.endness = VexEndnessLE;
     vta.arch_guest = (VexArch)dls->ti.tarch.ta;
     
     assert(arch.ta == dls->ti.tarch.ta);
@@ -264,41 +264,41 @@ bool runVEXOnBlobWithCallback(DecodeLibState *dls,
     vta.archinfo_host = dls->vai_guest;
     vta.archinfo_host.endness = vta.archinfo_guest.endness;
 
-	//vta.do_self_check = False;
-	vta.preamble_function = NULL;
+    //vta.do_self_check = False;
+    vta.preamble_function = NULL;
 
     vta.needs_self_check = needs_self_check;
-	vta.traceflags = 0; //-1;
-	vta.finaltidy = NULL;
+    vta.traceflags = 0; //-1;
+    vta.finaltidy = NULL;
 
     vta.disp_cp_chain_me_to_slowEP = (const void *)disp_cp_chain_me_to_slowEP;
     vta.disp_cp_chain_me_to_fastEP = (const void *)disp_cp_chain_me_to_fastEP;
     vta.disp_cp_xindir = (const void *)disp_cp_xindir;
     vta.disp_cp_xassisted = (const void *)disp_cp_xassisted;
 
-	bool did_throw=false;
-	if( dls->do_throw ) {
-		try {
-			vtr = LibVEX_Translate(&vta);
-		} catch( TranslateException &te) {
-			did_throw = true;
-		}
-	} else {
-		vtr = LibVEX_Translate(&vta);
-	}
+    bool did_throw=false;
+    if( dls->do_throw ) {
+        try {
+            vtr = LibVEX_Translate(&vta);
+        } catch( TranslateException &te) {
+            did_throw = true;
+        }
+    } else {
+        vtr = LibVEX_Translate(&vta);
+    }
 
-	if( !did_throw ) {
-		unsigned long read = vge.len[0];
-		if( (vtr.status == VexTranslateResult::VexTransOK) && (read > 0)) {
-			didGood = true;
-		}
+    if( !did_throw ) {
+        unsigned long read = vge.len[0];
+        if( (vtr.status == VexTranslateResult::VexTransOK) && (read > 0)) {
+            didGood = true;
+        }
 
-		*readBytes = read;
-	} else {
-		*readBytes = 0;
-	}
+        *readBytes = read;
+    } else {
+        *readBytes = 0;
+    }
 
-	return didGood;
+    return didGood;
 }
 
 bool convertToOneBlock( void *ctx,
@@ -309,12 +309,12 @@ bool convertToOneBlock( void *ctx,
                         unsigned int maxNumStmts,
                         BlockPtr &blockOut)
 {
-	OneBlockTransCtx obtc;
-	unsigned long nbytes;
-	DecodeLibState *dls = (DecodeLibState*)ctx;
-	bool result = true;
+    OneBlockTransCtx obtc;
+    unsigned long nbytes;
+    DecodeLibState *dls = (DecodeLibState*)ctx;
+    bool result = true;
 
-	obtc.curId = 0;
+    obtc.curId = 0;
     obtc.ta = arch;
     obtc.maxSize = maxNumStmts;
 
@@ -327,7 +327,7 @@ bool convertToOneBlock( void *ctx,
         buf = (uint8_t *)( ((ptrdiff_t)buf) | 1 ); 
     }
 
-	result = runVEXOnBlobWithCallback(  dls, 
+    result = runVEXOnBlobWithCallback(  dls, 
                                         buf, 
                                         bufLen, 
                                         baseAddr,
@@ -344,18 +344,18 @@ bool convertToOneBlock( void *ctx,
         blockOut = obtc.b;
     }
 
-	return result;
+    return result;
 }
 
-void * initDecodeLib(TargetInfo	ti, bool do_throw, bool dbg_spew) {
-	DecodeLibState	*dls = new DecodeLibState();
-	dls->ti = ti;
-	initVex(dls, do_throw, dbg_spew);
-	return dls;
+void * initDecodeLib(TargetInfo ti, bool do_throw, bool dbg_spew) {
+    DecodeLibState  *dls = new DecodeLibState();
+    dls->ti = ti;
+    initVex(dls, do_throw, dbg_spew);
+    return dls;
 }
 
 void * initDecodeLib2(TargetArch ta, bool do_throw, bool dbg_spew) {
-	DecodeLibState	*dls = new DecodeLibState();
+    DecodeLibState  *dls = new DecodeLibState();
     TargetInfo      ti;
 
     if( ta.ta == X86 ) {
@@ -376,15 +376,15 @@ void * initDecodeLib2(TargetArch ta, bool do_throw, bool dbg_spew) {
     ti.chaseThreshold = 10;
     ti.opLevel = FullOpt;
     ti.tarch = ta;
-	dls->ti = ti;
-	initVex(dls, do_throw, dbg_spew);
-	return dls;
+    dls->ti = ti;
+    initVex(dls, do_throw, dbg_spew);
+    return dls;
 }
 
 void finiDecodeLib(void *ctx) {
-	DecodeLibState	*dls = (DecodeLibState *)ctx;
-	delete dls;
-	return;
+    DecodeLibState  *dls = (DecodeLibState *)ctx;
+    delete dls;
+    return;
 }
 
 void initLibState(void) {
