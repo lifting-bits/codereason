0a16248fd007940a635f231ec506ba6050a137c3
Jay <jay@computerality.com>
2015-07-13 14:58:14 -0400

    Update vex install script to use ssh url for git to allow easier changes and also use because of sh problems on OS X.

--- install_vex.sh
+++ install_vex.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-git clone https://github.com/trailofbits/libvex.git
+git clone git@github.com:trailofbits/libvex.git
 cd libvex
 CFLAGS=-g VEX_INSTALL_DIR=./build make install
 cd ..
