3fc8a0831d3333ae3e2d1382d74295705ddd5bdc
Jay <jay@computerality.com>
2015-07-15 14:41:52 -0400

    reverting clone hack for travis.

--- install_vex.sh
+++ install_vex.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:trailofbits/libvex.git
+git clone https://github.com/trailofbits/libvex.git
+#git clone git@github.com:trailofbits/libvex.git
 cd libvex
 CFLAGS=-g VEX_INSTALL_DIR=./build make install
 cd ..
